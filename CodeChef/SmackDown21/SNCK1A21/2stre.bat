404716046fb33be0b86796621c5de75b131c7de4
OnMyZenith <asr3187@gmail.com>
2022-02-01 18:59:02 +0530

    cc

--- CodeChef/SmackDown21/SNCK1A21/2stre.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-for /l %%x in (1, 1, 99999) do (
-    echo %%x
-    gen2 %%x > in
-    EQBEAUTY < in > a_out 
-    abhaypp < in > abh_out 
-    fc /w abh_out a_out > diagnostics || exit /b
-)
-echo All Tests Passed!!
--- CodeChef/SmackDown21/SNCK1A21/stress.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-for /l %%x in (1, 1, 99999) do (
-    echo %%x
-    gen %%x > in
-    a < in > a_out 
-    validator
-    fc /w out t_out > diagnostics || exit /b
-)
-echo All Tests Passed!!
