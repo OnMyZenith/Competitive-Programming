d3e88fb1aa5ceeff41c12b06e5afc0cb40c6d2e0
OnMyZenith <asr3187@gmail.com>
2022-02-06 12:58:52 +0530

    Create vamos_fIO_fastIOandPrecision.cpp

---
+++ Templates/Snippets/vamos_fIO_fastIOandPrecision.cpp
@@ -0,0 +1,2 @@
+// For fast input outout, unsync scanf<->cin and untie cin cout and precision fix
+#define vamos ios_base::sync_with_stdio(false), cin.tie(nullptr), cout << setprecision(15) << fixed;
