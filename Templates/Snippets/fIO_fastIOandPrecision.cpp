65f8e9b7e0b12ea727761b5c7bef555c387c2a84
OnMyZenith <asr3187@gmail.com>
2022-02-06 12:58:48 +0530

    Delete fIO_fastIOandPrecision.cpp

--- Templates/Snippets/fIO_fastIOandPrecision.cpp
+++
@@ -1,7 +0,0 @@
-// For fast input outout, unsync scanf<->cin and untie cin cout and precision fix
-    ios_base::sync_with_stdio(false);
-// #ifndef asr_debug
-    cin.tie(nullptr);
-// #endif
-    cout << setprecision(15) << fixed;
-    
