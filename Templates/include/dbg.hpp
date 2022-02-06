62fb4200b41eef54ecd20456d86fae69a86ecbc0
OnMyZenith <asr3187@gmail.com>
2022-02-06 12:58:30 +0530

    Update dbg.hpp

--- Templates/include/dbg.hpp
+++ Templates/include/dbg.hpp
@@ -1,5 +1,8 @@
 #include "easyIO.hpp"
 
+#ifndef ___DBG
+#define ___DBG 1
+
 void DBG() { cerr << "]" << endl; }
 template <class T, class... U > void DBG(const T &t, const U &...u) {
     cerr << to_string(t);
@@ -10,3 +13,5 @@ template <class T, class... U > void DBG(const T &t, const U &...u) {
 #define dbg(...) cerr << "Line(" << __LINE__ << ") -> [" << #__VA_ARGS__ << "]: [", DBG(__VA_ARGS__)
 // #define chk(...) \
 //     if (!(__VA_ARGS__)) cerr << "Line(" << __LINE__ << ") -> function(" << __FUNCTION__ << ") -> CHK FAILED: (" << #__VA_ARGS__ << ")" << '\n', exit(0);
+
+#endif // ___DBG
