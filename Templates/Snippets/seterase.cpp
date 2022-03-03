42ac4157146119f1345c7a8ff02ed515063c0ed8
OnMyZenith <asr3187@gmail.com>
2022-03-03 11:36:15 +0530

    Update seterase.cpp

--- Templates/Snippets/seterase.cpp
+++ Templates/Snippets/seterase.cpp
@@ -2,6 +2,5 @@
 template <class T, class U> bool erase(T &t, const U &u) {
     auto it = t.find(u);
     if (it == end(t)) return false;
-    else t.erase(it);
-    return true;
+    t.erase(it); return true;
 }
