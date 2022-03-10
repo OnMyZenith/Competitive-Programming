5eb67444995c6b322fb9b6f5a0ce41e5ae55ab3a
OnMyZenith <asr3187@gmail.com>
2022-03-10 16:07:05 +0530

    Update seterase.cpp

--- Templates/Snippets/seterase.cpp
+++ Templates/Snippets/seterase.cpp
@@ -1,4 +1,5 @@
 // Confirm if deletion from set/multiset was successful
+// Because multiset.erase(ele) will erase all occurances of ele!!
 template <class T, class U> bool erase(T &t, const U &u) {
     auto it = t.find(u);
     if (it == end(t)) return false;
