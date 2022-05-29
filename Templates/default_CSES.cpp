781102cb97a1b62aa0d88722e7e506428e0c1eca
OnMyZenith <asr3187@gmail.com>
2022-05-29 17:42:35 +0530

    template

---
+++ Templates/default_CSES.cpp
@@ -0,0 +1,22 @@
+/* Author: OnMyZenith - https://github.com/OnMyZenith */
+#include <bits/extc++.h>
+using namespace std;
+using namespace __gnu_pbds;
+using ll = long long;
+
+#ifdef asr_debug
+#include "dbg.hpp"
+#else
+#define dbg(...) 007
+#endif
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout << setprecision(15) << fixed;
+
+    
+
+
+    return 0;
+}
--- Templates/include/home.hpp
+++ Templates/include/home.hpp
@@ -196,6 +196,32 @@ void ps(const T &t, const U &...u) { // print with spaces, with newline in the e
 }
 #endif // ___EASY_IO
 
+template <class T> using ord_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+/*
+Source : https://codeforces.com/blog/entry/11080
+Note on using less_equal as comparison function to use it as a multiset:
+    _GLIBCXX_DEBUG must not be defined, otherwise some internal check will fail.
+    find will always return end.
+    lower_bound works like upper_bound in normal set (to return the first element > it)
+    upper_bound works like lower_bound in normal set (to return the first element >= it)
+    find_by_order and order_of_key works properly (unlike the 2 functions above).
+
+Some more observations:
+    erase(ord_mul_set.find(ele)); <-- doesn't work
+    use this instead:
+    auto it = ord_mul_set.upper_bound(ele); <-- actually gives lower_bound
+    if(*it == ele) ord_mul_set.erase(it);
+*/
+template <class T> using ord_multiset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+template <typename T_vector> void ov(const T_vector &v, bool add_one = false, int start = -1, int end = -1) {
+    if (start < 0) start = 0;
+    if (end < 0) end = int(v.size());
+
+    for (int i = start; i < end; i++)
+        cout << v[i] + (add_one ? 1 : 0) << " \n"[i == end - 1];
+}
+
 
 #ifdef asr_debug
 #include "dbg.hpp"
