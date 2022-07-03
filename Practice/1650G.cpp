bfbb1523c789fbd25581cb97c4cafd0fb59a42e8
OnMyZenith <asr3187@gmail.com>
2022-07-03 09:51:22 +0530

    prac

--- Practice/1650G.cpp
+++ Practice/1650G.cpp
@@ -23,63 +23,75 @@ vector<vector<int>> reG(int n, int m, bool bi = 1) {
     return adj;
 }
 
-template <class T> void remDupf(vector<T> &v) { sort(v.begin(), v.end()), v.erase(unique(v.begin(), v.end()), end(v)); }
-
+template <class T> using pqdec = std::priority_queue<T>;
+template <class T> using pqinc = std::priority_queue<T, vector<T>, greater<T>>;
+const int INF = 1e9;
+const int MOD = 1e9 + 7;
 
 void solve() {
     int n, m, s, e; cin >> n >> m >> s >> e; s--, e--;
     auto adj = reG(n, m);
 
-    vector<bool> vis(n); vis[s] = 1;
-    queue<int> q; q.push(s);
-    set<int> q1;
-    vector<int> vis_curr, dis(n); int _dis = 0;
-    vector<vector<int>> p(n);
+    // best2[v].begin()->first == shortest path to v
+    // best2[v].begin()->second == number of ways to get to v with this path
+
+    // best2[v].rbegin()->first == 2nd shortest path to v
+    // best2[v].rbegin()->second == number of ways to get to v with this path
+    vector<set<pair<int, ll>>> best2(n);
+
+    pqinc<pair<int, int>> q; q.push({0, s});
+    best2[s].insert({0, 1});
+
     while (!q.empty()) {
-        int v = q.front(); q.pop();
-        for (auto u : adj[v]) {
-            if(!vis[u]) {
-                vis_curr.push_back(u);
-                p[u].push_back(v);
-                q1.insert(u);
-                dis[u] = _dis;
-            }
-        }
+        auto [d_v, v] = q.top(); q.pop();
+        auto it = best2[v].lower_bound({d_v, 0});
+        if (it == best2[v].end()) continue;
+        else assert(it->first == d_v);
+        ll ways_v = it->second;
 
-        if(q.empty()) {
-            for (auto &u : vis_curr) {
-                vis[u] = 1;
+        for (auto &u : adj[v]) {
+            if (best2[u].empty()) {
+                best2[u].insert({d_v + 1, ways_v});
+            } else {
+                it = best2[u].lower_bound({d_v + 1, 0});
+                if (it == best2[u].end()) {
+                    if ((int)best2[u].size() == 1) {
+                        if (best2[u].begin()->first < d_v) continue;
+                        else best2[u].insert({d_v + 1, ways_v});
+                    } else {
+                        continue;
+                        // dont pursue edge;
+                    }
+                } else {
+                    if (++it == best2[u].end()) {
+                        --it;
+                        if (it->first == d_v + 1) {
+                            ll tmp = it->second;
+                            best2[u].erase(it);
+                            best2[u].insert({d_v + 1, (tmp + ways_v) % MOD});
+                            continue;
+                        } else {
+                            best2[u].erase(it);
+                            best2[u].insert({d_v + 1, ways_v});
+                        }
+                    } else {
+                        --it;
+                        if (it->first == d_v + 1) {
+                            ll tmp = it->second;
+                            best2[u].erase(it);
+                            best2[u].insert({d_v + 1, (tmp + ways_v) % MOD});
+                            continue;
+                        } else {
+                            best2[u].erase(--best2[u].end());
+                            best2[u].insert({d_v + 1, ways_v});
+                        }
+                    }
+                }
             }
-            if(q1.count(e)) break;
-            for (auto &u : q1) {
-                q.push(u);
-            }
-            q1.clear(), vis_curr.clear(); _dis++;
+            q.push({d_v + 1, u});
         }
     }
-
-    vector<int> rightPath;
-
-    q = queue<int>(); q.push(e);
-    fill(vis.begin(), vis.end(), 0); vis[e] = 1;
-    while(!q.empty()) {
-        int v = q.front(); q.pop();
-        rightPath.push_back(v);
-
-        for (auto &u : p[v]) {
-            if(!vis[u]) {
-                vis[u] = 1;
-                q.push(u);
-            }
-        }
-    }
-
-    remDupf(rightPath);
-
-    
-
-
-
+    cout << (best2[e].begin()->second + ((int)best2[e].size() == 2 && (best2[e].rbegin()->first == best2[e].begin()->first + 1) ? best2[e].rbegin()->second : 0)) % MOD << '\n';
 }
 
 int main() {
