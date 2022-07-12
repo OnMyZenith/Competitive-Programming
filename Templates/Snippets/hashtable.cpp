eb9e9f66038d89f4c1fd9c94d2d6d0de680b0ae1
OnMyZenith <asr3187@gmail.com>
2022-07-13 01:58:02 +0530

    snips

--- Templates/Snippets/hashtable.cpp
+++ Templates/Snippets/hashtable.cpp
@@ -15,6 +15,25 @@ struct splitmix64_hash {
         static const uint64_t FIXED_RANDOM = std::chrono::steady_clock::now().time_since_epoch().count();
         return splitmix64(x + FIXED_RANDOM);
     }
+
+    size_t operator()(pair<uint64_t, uint64_t> x) const {
+        static const uint64_t FIXED_RANDOM = std::chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x.first + FIXED_RANDOM) ^ (splitmix64(x.second + FIXED_RANDOM) >> 1);
+    }
+
+    // // Comment everything above for this to work
+    // // Won't work with debug flags
+    // static __uint128_t splitmix64(__uint128_t x) {
+    //     // http://xorshift.di.unimi.it/splitmix64.c
+    //     x += 0x9e3779b97f4a7c15 * 0x9e3779b97f4a7c15;
+    //     x = (x ^ (x >> 60)) * 0xbf58476d1ce4e5b9 * 0xbf58476d1ce4e5b9;
+    //     x = (x ^ (x >> 57)) * 0x94d049bb133111eb * 0x94d049bb133111eb;
+    //     return x ^ (x >> 62);
+    // }
+    // __uint128_t operator()(__uint128_t x) const {
+    //     static const __uint128_t FIXED_RANDOM = std::chrono::steady_clock::now().time_since_epoch().count();
+    //     return splitmix64(x + FIXED_RANDOM);
+    // }
 };
 
 template <class T, class U, typename Hash = splitmix64_hash> using hash_map = gp_hash_table<T, U, Hash>;
