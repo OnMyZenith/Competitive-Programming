f90d24c07924d54ddc36c9dac44a773ade393882
OnMyZenith <asr3187@gmail.com>
2022-07-16 14:48:03 +0530

    Not finished yet

--- CSES/DynamicProgramming/CountingNumbers.cpp
+++ CSES/DynamicProgramming/CountingNumbers.cpp
@@ -1,7 +1,6 @@
 /* Author: OnMyZenith - https://github.com/OnMyZenith */
-#include <bits/extc++.h>
+#include "bits/stdc++.h"
 using namespace std;
-using namespace __gnu_pbds;
 using ll = long long;
 
 #ifdef asr_debug
@@ -10,27 +9,54 @@ using ll = long long;
 #define dbg(...) 007
 #endif
 
+const int MOD = 1e9 + 007; // 998244353;
+
+int modpow(int a, uint64_t b) {
+    int res = 1;
+    while (b) {
+        if (b & 1) res = (ll)res * a % MOD;
+        a = (ll)a * a % MOD;
+        b >>= 1;
+    }
+    return res;
+}
+
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
+    cin.tie(nullptr)->sync_with_stdio(false);
     cout << setprecision(15) << fixed;
 
-    vector<vector<int>> dp(19, vector<int>(10)); //dp[2][7] = cnt of 7s in 100s place
-
-    for (int i = 0; i < 10; i++) {
-        dp[0][i] = 1;
+    ll a, b;
+    cin >> a >> b;
+    vector<int> cnt(20);
+    for (int i = 0; i < 20; i++) {
+        // cnt[i] = (i ? cnt[i - 1] : 0) + modpow(9, i);
+        cnt[i] = modpow(9, i);
+        if (cnt[i] >= MOD) cnt[i] -= MOD;
     }
-    
-    for (int j = 0; j < 19; j++) {
-        for (int i = 0; i < 10; i++) {
-            dp[j][i] *= 9;
+
+    dbg(cnt);
+
+    auto calc = [&](ll x) {
+        int res = 0;
+        string s = to_string(x);
+        reverse(s.begin(), s.end());
+
+        int len = (int)s.length();
+        while (len--) {
+            int c = s.back() - '0';
+            s.pop_back();
+            res = (res + (ll)c * cnt[len] % MOD) % MOD;
         }
+        return res + 1;
+    };
+
+    for (int i = 0; i < 102; i++) {
+        cout << i << " :" << calc(i) << '\n';
     }
 
-    
+    dbg(calc(b));
+    dbg(calc(a));
 
-
-
-
+    cout << max(calc(b) - calc(a - 1), 0) << '\n';
     return 0;
 }
